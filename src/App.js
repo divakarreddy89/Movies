b7f863be2a35188aa1f7e3ff325026c812ff320f
divakarreddy89 <divakarreddy89@gmail.com>
2019-10-22 20:04:20 +0530

    concept practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import Movies from "./Movies/Movies";
+// import Movies from "./Movies/Movies";
 function App() {
-  return <div className="App">{<Movies />}</div>;
+  return (
+    <div className="App">
+      {
+        // <Movies />
+        <h1>New concepts</h1>
+      }
+    </div>
+  );
 }
 
 export default App;
